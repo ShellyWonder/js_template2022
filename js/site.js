3975186cd4d23586ee1c4f004bbfeb3632479fa2
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-01-17 16:35:37 -0600

    Added prism files & css

---
+++ js/site.js
@@ -0,0 +1,9 @@
+//display message to the user:
+function getMessage() {
+    let userMessage = document.getElementById("message").value;
+    Swal.fire(
+        'The Internet?',
+        'That thing is still around?',
+        'question'
+    )
+}
