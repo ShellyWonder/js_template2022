a910f5d808642e0bd20ddd93f7aa0978beee3802
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-01-19 14:24:06 -0600

    corrected errors

--- js/site.js
+++ js/site.js
@@ -1,9 +1,26 @@
-//display message to the user:
-function getMessage() {
-    let userMessage = document.getElementById("message").value;
-    Swal.fire(
-        'The Internet?',
-        'That thing is still around?',
-        'question'
-    )
+//display message from the user:
+function getValues() {
+    let userString = document.getElementById("revString").value;
+    let RevString = reverseAString(userString);
+    //display Reverse String
+    displayData(revString);
+}
+//LOGIC: (old generate )
+function reverseAString(userString) {
+    let revString = "",
+        //decrimenting for loop
+        for (let i = userString.length - 1; i >= 0; i--) {
+            revString += userString[index];
+
+            return revString;
+
+        }
+}
+//Display back to DOM
+function displayData(revString) {
+    document.getElementById("msg").innerHTML = `Your string is: ${revString}`;
+
+    document.getElementById("alert").classList.remove('invisible');
+    document.getElementById("alert").classList.remove("d-none");
+
 }
